fc18aeebd4beb102cdf3f298c119e799a9a4ecc4
Alvaro Cavalcanti <alvarovictor@gmail.com>
2017-09-24 16:11:10 -0300

    Initial UI

--- frontend/app/polls/polls.js
+++ frontend/app/polls/polls.js
@@ -15,6 +15,36 @@ angular.module('myApp.polls', ['ngRoute', 'ngResource'])
 
   vm.polls = [];
 
+  vm.createPoll = function () {
+    $resource('/api/polls').save().$promise.then(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    getPolls();
+  };
+
+  vm.castVote = function (pollId, optionName) {
+    console.log("castVote");
+    getPolls();
+  };
+
+  vm.closePoll = function (pollId) {
+    $resource('/api/polls/:pollId/close').save({pollId}, {}).$promise.then(
+      (poll) => {
+        console.log(poll)
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    getPolls();
+  };
+
   function getPolls() {
     $resource('/api/polls').query({}, {}).$promise.then(
       (polls) => {
