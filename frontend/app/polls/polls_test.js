6657e023758842764d95a5c2aeeae25345a8ffa1
Alvaro Cavalcanti <alvarovictor@gmail.com>
2017-10-01 16:32:15 -0300

    Added test for user input and post behavior

--- frontend/app/polls/polls_test.js
+++ frontend/app/polls/polls_test.js
@@ -33,7 +33,7 @@ describe("myApp.polls module", function() {
     it(
       "should define PollsCtrl",
       inject(function($controller) {
-        $httpBackend.expectGET('/api/polls')
+        $httpBackend.expectGET("/api/polls");
         var pollsCtrl = $controller("PollsCtrl");
         $httpBackend.flush();
         expect(pollsCtrl).toBeDefined();
@@ -43,10 +43,25 @@ describe("myApp.polls module", function() {
     it(
       "should fetch the polls from the backend upon creation",
       inject(function($controller) {
-        $httpBackend.expectGET('/api/polls')
+        $httpBackend.expectGET("/api/polls");
         var pollsCtrl = $controller("PollsCtrl");
         $httpBackend.flush();
         expect(pollsCtrl.polls.length).toBe(1);
+        expect(pollsCtrl.polls[0].id).toBe(1);
+        expect(pollsCtrl.polls[0].week).toBe(1);
+      })
+    );
+
+    it(
+      "should ask the user's name when casting a vote",
+      inject(function($controller) {
+        spyOn(window, "prompt").and.callFake(function() {
+          return "John";
+        });
+        var pollsCtrl = $controller("PollsCtrl");
+        $httpBackend.expectPOST("/api/polls/1/votes/John/pizzashack");
+        pollsCtrl.castVote(1, "pizzashack");
+        $httpBackend.flush();
       })
     );
   });
